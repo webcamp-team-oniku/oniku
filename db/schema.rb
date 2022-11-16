33903acc4a10041e0cdac77b965f9a501456745a
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-16 09:20:01 +0000

    ordersモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_084029) do
+ActiveRecord::Schema.define(version: 2022_11_16_090741) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,6 +52,20 @@ ActiveRecord::Schema.define(version: 2022_11_15_084029) do
     t.boolean "is_active", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_price", null: false
+    t.integer "payment_method", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
+    t.integer "#<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x00007f0681b4a940>"
   end
 
   create_table "users", force: :cascade do |t|
